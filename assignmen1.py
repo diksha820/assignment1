7304f57d7fc80a950e678a3832e755abdf9a9ca3
diksha <drkedar80558055@gmail.com>
2026-06-06 11:22:02 +0530

    add assignment1

--- assignmen1.py
+++ assignmen1.py
@@ -1,6 +1,19 @@
 #Q1
+"""
 num=int(input("enter a number : "))
-if num%2==0:
-    print("positive")
+if num>=0:
+    print(f"{num} is a positive")
 else:
-    print("negative")
+    print(f"{num} is a negative")
+
+"""
+#Q2
+num=int(input("enter a number :"))
+if num%2==0:
+    print(f"even number is {num}")
+else:
+    print(f"odd number is {num}")
+
+
+
+
